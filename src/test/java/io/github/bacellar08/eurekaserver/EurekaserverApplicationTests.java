d10239218f72141e3a31875be50449283e841faf
Alexandre Bacellar <alexandre.bacellar@gmail.com>
2024-06-17 10:46:19 -0300

    init

---
+++ src/test/java/io/github/bacellar08/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bacellar08.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
